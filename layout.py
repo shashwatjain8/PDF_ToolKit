d738e5b46cd63c4fb9d61af9679540b98d98dbd8
zero <shashwatjain8@gmail.com>
2018-10-09 23:17:05 +0530

    UI

---
+++ layout.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'main.ui'
+#
+# Created by: PyQt5 UI code generator 5.5.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+def fd():
+    return True
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(611, 439)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(10, 10, 99, 27))
+        self.pushButton.setObjectName("pushButton")
+        self.lblPDF = QtWidgets.QLabel(self.centralwidget)
+        self.lblPDF.setGeometry(QtCore.QRect(120, 10, 241, 31))
+        self.lblPDF.setAutoFillBackground(True)
+        self.lblPDF.setText("")
+        self.lblPDF.setObjectName("lblPDF")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(20, 60, 151, 17))
+        self.label_2.setObjectName("label_2")
+        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setGeometry(QtCore.QRect(10, 90, 91, 17))
+        self.label_3.setObjectName("label_3")
+        self.lblPages = QtWidgets.QLabel(self.centralwidget)
+        self.lblPages.setGeometry(QtCore.QRect(430, 10, 121, 31))
+        self.lblPages.setText("")
+        self.lblPages.setObjectName("lblPages")
+        self.textBrowser = QtWidgets.QTextBrowser(self.centralwidget)
+        self.textBrowser.setGeometry(QtCore.QRect(110, 90, 256, 21))
+        self.textBrowser.setObjectName("textBrowser")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 611, 25))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "PDF ToolKit"))
+        self.pushButton.setText(_translate("MainWindow", "Load a PDF"))
+        self.label_2.setText(_translate("MainWindow", "Extract Text from PDF"))
+        self.label_3.setText(_translate("MainWindow", "Enter Pages"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+
--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def ShowChoice():
 
 
 master.title("PDF ToolKit 1.0")
-master.iconbitmap('C:\\Users\\1000256289\\Documents\\Tasks\\STM_UT_Scenarios\\info.ico')
+
 v = IntVar()
 v.set(1)
 
@@ -27,11 +27,14 @@ l.grid(row =0,padx =5, pady =5, columnspan = 2)
 b1 = Button(master, text='Load a PDF', command=loadFile)
 b1.grid(row=1, column=0, sticky=W,padx =5, pady =5)
 b1.config(width = 20 )
+b2 = Button(master, text='Extract Text', command=loadFile)
+b2.grid(row=1, column=0, sticky=W,padx =5, pady =5)
+b2.config(width = 20 )
 
 l1 = Label(master, text="")
 l1.grid(row=1,column=1, padx =5, pady =5)
 l1.config(width = 20 )
-l2 = Label(master, text="Enter Test Case Number")
+l2 = Label(master, text="")
 l2.grid(row=3, padx =5, pady =5)
 l2.config(width = 20 )
 
